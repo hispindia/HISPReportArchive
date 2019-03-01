44e6caca14c7cefef0847488fc8af73c2bf27f8e
Vicky Rohilla <rohillavicky.172@gmail.com>
2019-03-01 13:12:40 +0530

    Updated Date

--- OutputReports/DHIS2 Reports/HTML Reports/UPHMIS/RAW_DATA_REPORTS(SQL_REPORTS)/UPHMISRawDataReportFeburary2019.sql
+++ OutputReports/DHIS2 Reports/HTML Reports/UPHMIS/RAW_DATA_REPORTS(SQL_REPORTS)/UPHMISRawDataReportFeburary2019.sql
@@ -2023,4 +2023,4 @@ from
 group by
    state , div, dis,dishmiscode, block, blockhmiscode, facility, facilityhmiscode,organisationunitid, NIN, tp, st, loc
 order by
-   state , div, dis, block, facility) TO '/DATA/DHIS/Apps/dhis/tomcat/dhis_home/RDreport/script/sql/Gen/UPHMISRawDataReportFebruary18.csv' (format CSV, HEADER);
+   state , div, dis, block, facility) TO '/DATA/DHIS/Apps/dhis/tomcat/dhis_home/RDreport/script/sql/Gen/UPHMISRawDataReportFebruary19.csv' (format CSV, HEADER);
